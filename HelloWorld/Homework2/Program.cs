13c3675bb476cb36dc8587340c0d8871538908a4
Michael Bowen <mikee.bowen@gmail.com>
2020-04-15 18:00:29 -0700

    add homework 2 console application

---
+++ HelloWorld/Homework2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework2.Models;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>
+            {
+                new User { Name = "Dave", Password = "hello" },
+                new User { Name = "Steve", Password = "steve" },
+                new User { Name = "Lisa", Password = "hello" }
+            };
+
+            users.FindAll(u =>
+            {
+                if (u.Password == "hello")
+                {
+                    Console.WriteLine(u.Password);
+                }
+                return u.Password == "hello";
+            });
+            users.RemoveAll(u => u.Password == u.Name.ToLower());
+            users.Remove(users.Find(u => u.Password == "hello"));
+            users.FindAll(u =>
+            {
+                Console.WriteLine($"{u.Name}: {u.Password}");
+                return true;
+            });
+        }
+    }
+}
