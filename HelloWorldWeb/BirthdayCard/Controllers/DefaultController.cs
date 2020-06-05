847dc33d00e875c9f3804203abb7be309b46a902
Michael Bowen <mikee.bowen@gmail.com>
2020-06-04 19:32:25 -0700

    assignment 6

---
+++ HelloWorldWeb/BirthdayCard/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayCard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private bool creating = true;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(BirthdayMessage birthdayMessage)
+        {
+            if (ModelState.IsValid && creating)
+            {
+                creating = !creating;
+                return View(birthdayMessage);
+            }
+            if (!creating)
+            {
+                creating = !creating;
+                ModelState.Clear();
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ HelloWorldWeb/BirthdayCard/Models/BirthdayMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayMessage
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+    }
+}
